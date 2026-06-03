613900581a148315127c99e3cb705c1d7a4bb155
Chris Larsen <clarsen12@gmail.com>
2026-06-03 15:50:40 -0600

    Restructure

---
+++ tests/login/login.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/login/loginPage";
+
+test("login without page object test", async ({ page }) => {
+  await page.goto("https://practicesoftwaretesting.com/");
+  await page.locator('[data-test="nav-sign-in"]').click();
+  await page
+    .locator('[data-test="email"]')
+    .fill("customer@practicesoftwaretesting.com");
+  await page.locator('[data-test="password"]').fill("welcome01");
+  await page.locator('[data-test="login-submit"]').click();
+  await expect(page.locator('[data-test="nav-menu"]')).toContainText(
+    "Jane Doe",
+  );
+  await expect(page.locator('[data-test="page-title"]')).toContainText(
+    "My account",
+  );
+});
+
+test("login with page object test", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
+  await expect(page.getByTestId("nav-menu")).toContainText("Jack Howe");
+});
